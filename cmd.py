71469fc18d424089ba08a3ee64398d938195b18b
TKTK-ST <kaiketsu.genki@gmail.com>
2018-08-16 11:51:08 +0900

    if __name__ == '__main__' no

--- cmd.py
+++ cmd.py
@@ -17,41 +17,42 @@ numbers = ['0','1','2','3','4','5','6','7','8','9']
 use = alphabets
 
 print()
-password = getpass.getpass('Please set a password of ' + str(maxlength) + 'characters or less.\n' + str(maxlength) + '文字以下のパスワードを設定してください\n\nPassword:')
-print()
-if (len(password) > maxlength) or (len(password) < mixlength):
+if __name__ == '__main__':
+    password = getpass.getpass('Please set a password of ' + str(maxlength) + 'characters or less.\n' + str(maxlength) + '文字以下のパスワードを設定してください\n\nPassword:')
+    print()
+    if (len(password) > maxlength) or (len(password) < mixlength):
     print(Fore.RED + 'Password must be ' + str(mixlength) + ' to ' + str(maxlength) + ' characters.\nパスワードは' + str(mixlength) + '文字以上、' + str(maxlength) + '文字以下' + 'で設定して下さい')
-else:
-    for a in password:
-        if a in use:
-            continue
-        else:
-            print(Fore.RED + 'The password contains characters that can not be used\nパスワードに使用できない文字が含まれています')
-            break
     else:
-        if password == getpass.getpass('Please enter your password again for confirmation.\n確認の為、もう一度パスワードを入力してください\n\nPassword:'):
-            print()
-            for n in range(1,maxlength + 1):
-                for x in itertools.product(use,repeat=n):
-                    result = ''.join(x)
-                    if password == result:
-                        sys.stdout.write("\r")
-                        #sys.stdout.flush()
-                        sys.stdout.write(Fore.GREEN + 'As a result of a brute force attack,we found that the password is "{}".'.format(result))
-                        print()
-                        #sys.stdout.flush()
-                        break
-                    elif result_view:
-                        sys.stdout.write('\r' + Fore.RED + 'Password did not match! : ' + '{}'.format(result))
-                        #sys.stdout.flush()
-                else:
-                  continue
-                break
+        for a in password:
+            if a in use:
+                continue
             else:
-                sys.stdout.write('\r' + Fore.RED + 'As a result of a brute force attack,we can not found password.')
-
+                print(Fore.RED + 'The password contains characters that can not be used\nパスワードに使用できない文字が含まれています')
+                break
         else:
-            print()
-            print(Fore.RED + 'It did not match the first password.\n最初のパスワードと一致しませんでした')
+            if password == getpass.getpass('Please enter your password again for confirmation.\n確認の為、もう一度パスワードを入力してください\n\nPassword:'):
+                print()
+                for n in range(1,maxlength + 1):
+                    for x in itertools.product(use,repeat=n):
+                        result = ''.join(x)
+                        if password == result:
+                            sys.stdout.write("\r")
+                            #sys.stdout.flush()
+                            sys.stdout.write(Fore.GREEN + 'As a result of a brute force attack,we found that the password is "{}".'.format(result))
+                            print()
+                            #sys.stdout.flush()
+                            break
+                        elif result_view:
+                            sys.stdout.write('\r' + Fore.RED + 'Password did not match! : ' + '{}'.format(result))
+                            #sys.stdout.flush()
+                    else:
+                        continue
+                    break
+                else:
+                    sys.stdout.write('\r' + Fore.RED + 'As a result of a brute force attack,we can not found password.')
 
-print()
+            else:
+                print()
+                print(Fore.RED + 'It did not match the first password.\n最初のパスワードと一致しませんでした')
+
+    print()
